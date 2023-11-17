9b474f2a591c8fa91631b031f94246b3fe05da72
luiz henrique <luizaaaljk2009@gmail.com>
2023-11-16 21:45:36 -0300

    projeto criado

---
+++ src/main/java/com/treinamento1/treino1/Treino1Application.java
@@ -0,0 +1,13 @@
+package com.treinamento1.treino1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Treino1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Treino1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/treinamento1/treino1/Treino1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treinamento1.treino1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Treino1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
